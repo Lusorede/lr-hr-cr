d224043794eab0faea9d266d0efbec982ee97685
hr-lusorede <66695819+hr-lusorede@users.noreply.github.com>
2020-06-17 16:00:40 +0100

    Car configuration

--- bt-strandtest.py
+++ bt-strandtest.py
@@ -33,16 +33,6 @@ GPIO.setwarnings(False)
 GPIO.setup(LED,GPIO.OUT)  #initialize GPIO21 (LED) as an output Pin
 GPIO.output(LED,0)
 
-def car(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
 
 def clear():
    for i in range(strip.numPixels()):
@@ -97,7 +87,50 @@ def carconfig():
        strip.setPixelColor(i, 0x0000FF)
    for i in range (scyi,scye):
        strip.setPixelColor(i, 0xFFFF00)
+   strip.show()
+def sred():
+   for i in range (scri,scre):
+       strip.setPixelColor(i, 0xFF0000)
+   for i in range (scgi,scge):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scbi,scbe):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scyi,scye):
+       strip.setPixelColor(i, 0x000000)
+   strip.show()   
+def sgreen():
+   for i in range (scri,scre):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scgi,scge):
+       strip.setPixelColor(i, 0x00FF00)
+   for i in range (scbi,scbe):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scyi,scye):
+       strip.setPixelColor(i, 0x000000)
    strip.show()  
+def sblue():
+   for i in range (scri,scre):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scgi,scge):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scbi,scbe):
+       strip.setPixelColor(i, 0x0000FF)
+   for i in range (scyi,scye):
+       strip.setPixelColor(i, 0x000000)
+   strip.show()  
+def syellow():
+   for i in range (scri,scre):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scgi,scge):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scbi,scbe):
+       strip.setPixelColor(i, 0x000000)
+   for i in range (scyi,scye):
+       strip.setPixelColor(i, 0xFFFF00)
+   strip.show() 
+
+
+
 
 
 server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
@@ -138,20 +171,244 @@ while 1:
    scye = 0
    print ("Reset Car Configuration")
 #Car configuration
- if (data == "cars12"):    
-  print ("R4-G3-B1-Y1")
+ if (data == "cars01"):    
+  print ("R9-G0-B0-Y0")
   scrdi = 0  
-  scre = 30
-  scgi = 31  
-  scge = 40
-  scbi = 41 
-  scbe = 50
-  scyi = 51 
-  scye = 54
-  print scye
+  scre = 54
+  scgi = 0  
+  scge = 0
+  scbi = 0 
+  scbe = 0
+  scyi = 0 
+  scye = 0
+  configlabel = data
   carconfig()
-
+ if (data == "cars02"):    
+  print ("R8-G1-B0-Y0")
+  scrdi = 0  
+  scre = 48
+  scgi = 48 
+  scge = 54
+  scbi = 0 
+  scbe = 0
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars03"):    
+  print ("R7-G2-B0-Y0")
+  scrdi = 0  
+  scre = 39
+  scgi = 39 
+  scge = 54
+  scbi = 0
+  scbe = 0
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars04"):    
+  print ("R7-G1-B1-Y0")
+  scrdi = 0  
+  scre = 42
+  scgi = 42 
+  scge = 48
+  scbi = 48
+  scbe = 54
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars05"):    
+  print ("R6-G3-B0-Y0")
+  scrdi = 0  
+  scre = 33
+  scgi = 33
+  scge = 54
+  scbi = 0
+  scbe = 0
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars06"):    
+  print ("R6-G2-B1-Y0")
+  scrdi = 0  
+  scre = 33
+  scgi = 33
+  scge = 45
+  scbi = 45
+  scbe = 54
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars07"):    
+  print ("R6-G1-B1-Y1")
+  scrdi = 0  
+  scre = 33
+  scgi = 33
+  scge = 39
+  scbi = 39
+  scbe = 45
+  scyi = 45
+  scye = 54
+  configlabel = data
+  carconfig()
+ if (data == "cars08"):    
+  print ("R5-G4-B0-Y0")
+  scrdi = 0  
+  scre = 27
+  scgi = 27
+  scge = 54
+  scbi = 0
+  scbe = 0
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars09"):    
+  print ("R5-G3-B1-Y0")
+  scrdi = 0  
+  scre = 27
+  scgi = 27
+  scge = 45
+  scbi = 45
+  scbe = 54
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars10"):    
+  print ("R5-G2-B2-Y0")
+  scrdi = 0  
+  scre = 27
+  scgi = 27
+  scge = 39
+  scbi = 39
+  scbe = 54
+  scyi = 0 
+  scye = 0
+  configlabel = data
+  carconfig()
+ if (data == "cars11"):    
+  print ("R5-G2-B1-Y1")
+  scrdi = 0  
+  scre = 27
+  scgi = 27
+  scge = 39
+  scbi = 39
+  scbe = 45
+  scyi = 45
+  scye = 54
+  configlabel = data
+  carconfig()
+ if (data == "cars12"):    
+   print ("R4-G4-B1-Y0")
+   scrdi = 0  
+   scre = 21
+   scgi = 21  
+   scge = 45
+   scbi = 45 
+   scbe = 54
+   scyi = 0 
+   scye = 0
+   configlabel = data
+   carconfig()  
+ if (data == "cars13"):    
+   print ("R4-G3-B2-Y0")
+   scrdi = 0  
+   scre = 21
+   scgi = 21  
+   scge = 39
+   scbi = 39 
+   scbe = 54
+   scyi = 0 
+   scye = 0
+   configlabel = data
+   carconfig()
+ if (data == "cars14"):    
+   print ("R4-G3-B1-Y1")
+   scrdi = 0  
+   scre = 21
+   scgi = 21  
+   scge = 39
+   scbi = 39 
+   scbe = 45
+   scyi = 45
+   scye = 54
+   configlabel = data
+   carconfig()
+ if (data == "cars15"):    
+   print ("R4-G2-B2-Y1")
+   scrdi = 0  
+   scre = 21
+   scgi = 21  
+   scge = 33
+   scbi = 33 
+   scbe = 45
+   scyi = 45
+   scye = 54
+   configlabel = data
+   carconfig()   
+ if (data == "cars16"):    
+   print ("R3-G3-B3-Y0")
+   scrdi = 0  
+   scre = 16
+   scgi = 16  
+   scge = 33
+   scbi = 33 
+   scbe = 54
+   scyi = 0
+   scye = 0
+   configlabel = data
+   carconfig()   
+ if (data == "cars17"):    
+   print ("R3-G3-B2-Y1")
+   scrdi = 0  
+   scre = 16
+   scgi = 16  
+   scge = 33
+   scbi = 33 
+   scbe = 45
+   scyi = 45
+   scye = 54
+   configlabel = data
+   carconfig()   
+ if (data == "cars18"):    
+   print ("R3-G2-B2-Y2")
+   scrdi = 0  
+   scre = 16
+   scgi = 16  
+   scge = 27
+   scbi = 27 
+   scbe = 39
+   scyi = 39
+   scye = 54
+   configlabel = data
+   print configlabel
+   carconfig()      
+   
 ######
+ if (data == "start"):    
+   clear()
+   print configlabel + " shop start"
+ if (data == "sred"):    
+   sred()
+   print configlabel + " Red"
+ if (data == "sblue"):    
+   sblue()
+   print configlabel + " Blue"
+ if (data == "sgreen"):    
+   sgreen()
+   print configlabel + " Green"
+ if (data == "syellow"):    
+   syellow()
+   print configlabel + " Yellow"
+ if (data == "finalized"):    
+   carconfig()
+   print configlabel + " shop finalized"   
+##############################
  if (data == "0"):    #if '0' is sent from the Android App, turn OFF the LED
   print ("GPIO 21 LOW, LED OFF")
   GPIO.output(LED,0)
